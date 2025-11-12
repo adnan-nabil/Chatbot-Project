85c08c4aabf6731eef20b6b70c8f43dad694f6f9
Nabil Adnan <nabiladnan161@gmail.com>
2025-11-12 16:54:25 +0600

    initial

---
+++ actions/actions.py
@@ -0,0 +1,46 @@
+
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+     def name(self) -> Text:
+         return "action_hello_world"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+         dispatcher.utter_message(text="Hello World!")
+
+         return []
+
+
+# search Product
+class ActionCheckProduct(Action):
+    def name(self):
+        return "action_check_product"
+
+    def run(self, dispatcher, tracker, domain):
+        product = tracker.get_slot("product")
+        # Simulate API Hit (replace with real API call)
+        dispatcher.utter_message(text=f"I am checking the product '{product}' for you...")
+        return []
+    
+
+# full order placement
+class ActionPlaceOrder(Action):
+    def name(self):
+        return "action_place_order"
+
+    def run(self, dispatcher, tracker, domain):
+        product = tracker.get_slot("product")
+        # Simulate Order Placement (replace with real order logic)
+        dispatcher.utter_message(text=f"Your order for '{product}' has been placed successfully!")
+        return []    
+
+
